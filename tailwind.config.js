aeb81b995359ab37194cfb4de34a20426093148f
Sergey Kuleshov <sergey@kuleshov.online>
2022-12-28 10:57:02 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      backgroundImage: {
+        'waves': "url('../images/guilloche-background.png')",
+        'waves-dark': "url('../images/results-waves.png')",
+      }
+    },
+    colors: {
+      ...colors,
+      gold: '#d4b287',
+      'gold-dark': '#af916c',
+      'main-blue': '#003c55',
+    },
+  },
+  plugins: [],
+}
